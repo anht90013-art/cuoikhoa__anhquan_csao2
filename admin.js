b8e91810cff3b4222adf909cefd3d0265b504f5f
anht90013-art <anht90013@gmail.com>
2026-09-06 08:21:10 +0700

    a

---
+++ admin.js
@@ -0,0 +1,6 @@
+window.goToAddFilmPage = function () {
+  window.location.href = "addfilm.html";
+};
+
+window.saveName = window.goToAddFilmPage;
+
---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsofKod7--tuEF8uY8Yd4gMburUtgrXuI",
+  authDomain: "cuoikhoa-30bc8.firebaseapp.com",
+  projectId: "cuoikhoa-30bc8",
+  storageBucket: "cuoikhoa-30bc8.firebasestorage.app",
+  messagingSenderId: "875258442464",
+  appId: "1:875258442464:web:6b895df283d1bb7b3a1685",
+  measurementId: "G-GYJE5PP8QV"
+};
